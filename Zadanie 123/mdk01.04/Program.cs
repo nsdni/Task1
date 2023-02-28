56ec2d699a967c12becf36c54e05827400ba7960
nsdni <grenfdi2@gmail.com>
2023-02-28 10:52:55 +0300

    1

--- Zadanie 123/mdk01.04/Program.cs
+++ Zadanie 123/mdk01.04/Program.cs
@@ -7,64 +7,40 @@ class Person
     string fullName;
     int age;
 
-    public void move()
+    public void move(string Person)
     {
-        Console.WriteLine("Такой-то Person двигается");
+        Console.WriteLine("Такой-то " + Person + " двигается");
     }
 
-    public void talk()
+    public void talk(string Person)
     {
-        Console.WriteLine("Такой-то Person говорит");
+        Console.WriteLine("Такой-то " + Person + " говорит");
     }
+
     public Person()
     {
-        fullName = "any";
-        age = 0;
+        
     }
-    public Person(int i, string n)
+
+    public Person(string fullName, int age)
     {
-        fullName = n;
-        age = i;
+        this.fullName = fullName;
+        this.age = age;
     }
 
     static void Main(string[] args)
     {
-        Person obj = new Person();
-        obj.talk();
-
-        obj = new Person();
-        obj.move();
+        Person Vanyok = new Person("Ванёк", 17);
+        Vanyok.talk("Ванёк");
+        Vanyok.move("Ванёк");
+        Person Miwa = new Person("Миша", 17);
+        Miwa.talk("Миша");
+        Miwa.move("Миша");
     }
 }
 
 
 
-public class Sesons
-{
-    private String[] seasons = new String[] { "Зима", "Весна", "Лето", "Осень" };
-    public String favoriteSeason = "переходный сезон, когда заметно уменьшение светового дня, и постепенно понижается температура окружающей среды.";
 
 
 
-    public static void Seas(string[] args)
-    {
-        Console.WriteLine("Зима - 1\n Весна - 2\n Лето - 3\n Осень - 4");
-        Console.Write("month = ");
-        string month = Console.ReadLine();
-        switch (month)
-        {
-            case "1":
-                Console.WriteLine("Я люблю зиму");
-                break;
-            case "2":
-                Console.WriteLine("Я люблю весну");
-                break;
-            case "3":
-                Console.WriteLine("Я люблю лето");
-                break;
-            case "4":
-                Console.WriteLine("Я люблю осень");
-                break;
-        }
-    }
-}
---
+++ Zadanie 123/task2/task2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Phone
+{
+    public string number;
+    public string model;
+    public string weight;
+
+
+
+    public void receiveCall(string name)
+    {
+        Console.WriteLine("Звонит" + name);
+    }
+
+    public void getNumber(string number)
+    {
+        Console.Write(number);
+    }
+
+    public Phone(string number, string model, string weight)
+    {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+        Console.Write(number);
+        Console.Write(weight);
+        Console.Write(model);
+    }
+
+    public Phone(string number, string model)
+    {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone()
+    {
+
+    }
+    public void receiveCall(string name, string number)
+    {
+        Console.WriteLine(name + number);
+    }
+    public void sendMessege(string[] numbers)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    }
+}
+
